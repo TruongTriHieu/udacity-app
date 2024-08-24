4df1c239b92cf3b911d7368f29ad318b645020ab
TruongHieu <TruongTriHieu@gmail.com>
2024-08-24 13:58:56 +0700

    fix comment review

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,16 +7,23 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 if not app.debug:
-    handler = logging.FileHandler('app.log')
-    handler.setLevel(logging.WARNING) 
+    # Setup file handler for logging
+    handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+app.logger.addHandler(console_handler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,6 +67,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning(f'Login failed for username: {form.username.data} - Invalid credentials')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
